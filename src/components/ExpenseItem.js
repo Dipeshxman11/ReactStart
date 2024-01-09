7a8ebf950aef761276f0a3c9990fab0cb678ca19
Dipeshxman11 <dipeshramane9378>
2024-01-09 20:06:00 +0530

    "prpos passing data"

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,22 @@
+import "./ExpenseItem.css"
 
- function ExpenseItem (){
-    return  <div> 
-    <h2>Expense Items</h2>
-    <h3> Food Rs 10</h3>
-    <h3> Petrol Rs 100</h3>
-    <h3> Movies Rs 200</h3>
-    </div>
- }
+function ExpenseItem(props) {
+    return (
+        <div>
 
- export default ExpenseItem;
+            <div className="expense-item">
+                <div>
+                    {props.date.toISOString()}
+                </div>
+                <div className="expense-item__description">
+                    <h2> {props.title} </h2>
+                    <h2> {props.LocationOfExpenditure} </h2>
+                    <div className="expense-item__price">  Rs {props.amount} </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ExpenseItem;
