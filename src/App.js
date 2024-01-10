b8f9bcd829f164675b7578001a751c7ed938903b
Dipeshxman11 <dipeshramane9378>
2024-01-10 21:45:39 +0530

    "Form component created"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
+import React  from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+
 
 function App() {
   const expenses = [
@@ -36,7 +39,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./ExpenseForm.css"
+
+
+function ExpenseForm() {
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value)
+    }
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__controls">
+                    <label> Title </label>
+                    <input type="text"  onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__controls">
+                    <label> Amount </label>
+                    <input type="number" min="0.01" step="0.01" />
+                </div>
+                <div className="new-expense__controls">
+                    <label> Date </label>
+                    <input type="date" min="2024-10-01" max="2026-01-01" />
+                </div>
+            </div>
+            <div className="new-expense__actions"> 
+            <button className="submit"> Add Expense </button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm;
