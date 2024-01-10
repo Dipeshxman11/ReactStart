47a9f9ce13db71f5b400db36ddcf55cb61b88810
Dipeshxman11 <dipeshramane9378>
2024-01-10 20:43:29 +0530

    "react dom manipulated using useState"

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,27 @@
-import  ExpenseDate from "./ExpenseDate"
-import ExpenseDetails from "./ExpenseDetails";
-import Card from '../UI/Card';
-import "./ExpenseItem.css"
+    import React ,{ useState } from "react"
+    import  ExpenseDate from "./ExpenseDate"
+    import ExpenseDetails from "./ExpenseDetails";
+    import Card from '../UI/Card';
+    import "./ExpenseItem.css"
 
 
-function ExpenseItem(props) {
+    function ExpenseItem(props) {
+        const [amount, setAmount] = useState(props.amount)
 
-    const clickHandler = () => {
-        console.log("deleted")
+        const clickHandler = () => {
+            setAmount("$ 100")
+            console.log("deleted")
+        }
+
+        return (
+        
+                <Card className="expense-item">
+                <ExpenseDate  date={props.date}/>
+                    <ExpenseDetails amount={amount} LocationOfExpenditure={props.LocationOfExpenditure} title={props.title} /> 
+                <button onClick={clickHandler}> Delete </button>
+                </Card>
+
+        )
     }
 
-    return (
-       
-            <Card className="expense-item">
-               <ExpenseDate  date={props.date}/>
-                <ExpenseDetails amount={props.amount} LocationOfExpenditure={props.LocationOfExpenditure} title={props.title} /> 
-            <button onClick={clickHandler}> Delete </button>
-            </Card>
-
-    )
-}
-
-export default ExpenseItem;
+    export default ExpenseItem;
